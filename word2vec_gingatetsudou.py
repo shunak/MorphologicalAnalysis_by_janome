83835b022660aa82dad4b5d318d4120d9f8fd1de
DESKTOP-BRIGC4V\USER <shuna966@gmail.com>
2018-07-28 00:33:51 +0900

    180727

--- word2vec_gingatetsudou.py
+++ word2vec_gingatetsudou.py
@@ -26,7 +26,7 @@ lines = text.split("\r\n")
 
 # print(lines)
 for line in lines:
-    s = lines
+    s = line
     s = s.replace('|','') # |を消去　
     s = re.sub(r'《.+?》','',s) #ルビをとる　re.subで正規表現のsubstitute(置換)
     s = re.sub(r'[#.+?]','',s) #入力注をとる
@@ -41,7 +41,33 @@ for line in lines:
         hinsi = ps.split(',')[0] #品詞情報の先頭を取得
         if hinsi in ['名詞','形容詞','動詞','記号']: # 品詞がこれらのものなら、
             r.append(w) # リストの末尾に追加する
-        rl = (" ".join(r)).strip() # スペースで連結した文字列を作成
+    rl = (" ".join(r)).strip() # スペースで連結した文字列を作成 ループを抜けたあとに行う
+    results.append(rl)
+
+    # 得られた分かち書きの結果をモデルデータとして書き出して、その後、ベクトル化して処理をしていく
+
+    # モデルファイルに書き出す
+    wakachigaki_file = "gingatetudou.wakachi"
+    with open(wakachigaki_file, 'w',encoding='utf-8') as fp: # 'w'の指定で書き込み可能　as fpでfp(ファイルポインタ)という名称で扱えるようにする
+        fp.write("\n".join(results)) # 改行コードを入れて先ほどの結果を書き込んでいく。これで、unix形式のファイルにutf-8でデータが保存されていく
+
+    # word2vecでえられた単語一覧データをベクトル化していく
+    # 詳細 https://radimrehurek.com/gensim/models/word2vec.html
+data = word2vec.LineSentence(wakachigaki_file)
+model = word2vec.Word2Vec(data,size=200, window=10, hs=1, min_count=2, sg =1)
+model.save('gingatetudou.model')
+# print('Completed')
+
+
+print(model.most_similar(positive=['カムパネルラ']))
+
+
+
+
+
+
+
+    # print(rl)
 
 
 
